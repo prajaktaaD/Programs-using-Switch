6f83f81c37cea222a3afb27529ba564700d5177d
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-08 16:15:06 +0530

    Create Switch2.c
    Write a program to accept a number and print week days accordingly (monday-1....sunday-7)

---
+++ Switch2.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+  int choice;
+ 
+  
+ 
+  printf("\nEnter choice between 1 to 7:");
+  scanf(" %d",&choice);
+  switch(choice)
+  
+ {
+  case 1:printf("Monday");
+           break;
+ 
+  case 2:printf("Tuesday");
+           break;
+          
+ case 3:printf("Wednesday");
+           break;
+           
+ case 4:printf("Thrusday");
+           break;
+           
+ case 5:printf("Friday");
+           break;
+           
+ case 6:printf("Saturday");
+           break;
+           
+ case 7:printf("Sunday");
+           break;
+           
+ }
+ 
+ return 0;
+ 
+ }
+           
+ 
