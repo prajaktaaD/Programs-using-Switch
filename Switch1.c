16519ae25e5e0532ef6e219e963d7c184ecd9f46
prajaktaaD <70463406+prajaktaaD@users.noreply.github.com>
2020-11-08 16:01:10 +0530

    Create Switch1.c

---
+++ Switch1.c
@@ -0,0 +1,35 @@
+//1.Write menu driven caculator program (+.Addition,-.Subtraction,/.Division,*.Multiplication)
+
+
+#includE<stdio.h>
+int main()
+{
+  char ch;
+  int a,b;
+  printf("\nMenu:\n+.Addition\n-.Subtraction,\n\.Division,\n*.Multiplication");
+  printf("\nEnter two numbers:");
+  scanf("%d%d",&a,&b);
+  printf("\nEnter option:");
+  scanf("%c",&ch);
+  switch(ch)
+  
+ {
+  case '+':printf("sum=%d",a+b);
+           break;
+ 
+  case '-':printf("difference=%d",a-b);
+           break;
+          
+ case '\':printf("quotient=%d",a/b);
+           break;
+
+ case '*':printf("product=%d",a*b);
+           break;
+ }
+ 
+ return 0;
+ 
+ }
+           
+
+ 
